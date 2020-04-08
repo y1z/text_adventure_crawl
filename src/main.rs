9596953a7ddafd6323eb31c339ac04972ee16309
YB <whyis2cool@gmail.com>
2020-04-08 18:47:40 -0500

    Create room struct.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
 fn main() {
     println!("Hello, world!");
-    println!("just adding this to merge the current barch with the master");
 }
---
+++ src/room.rs
@@ -0,0 +1,7 @@
+
+struct room
+{
+    String description,
+    u32 x_pos,
+    u32 y_pos
+}
